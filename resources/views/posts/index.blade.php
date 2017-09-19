c94fd751def68d5bf28e4df3dacba0f02c03d52f
Ghost <avs.adh96@gmail.com>
2017-09-19 19:25:56 +0545

    Changed mate

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,7 +13,7 @@
 
 						<div class="col-md-8 col-sm-8">
 							<h3><a href="/posts/{{$post->id}}"> {{$post->title}}</a></h3>
-							<small>Written on {{$post->created_at}} &nbsp by &nbsp {{$post->user->name}}</small>
+							<small>Written on {{$post->created_at}}  by  {{$post->user->name}}</small>
 						</div>
 
 				</div>
